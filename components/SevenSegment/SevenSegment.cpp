a6ac94ccb215f109c5fb0567716eb24d9e41c97c
AnchisaPhetnoi <65030289@gmail.com>
2024-08-15 16:07:21 +0700

    LAB_7

--- components/SevenSegment/SevenSegment.cpp
+++ components/SevenSegment/SevenSegment.cpp
@@ -30,4 +30,87 @@ void SevenSegment::DisplayBlank()
     seg_e.OFF();
     seg_f.OFF();
     seg_g.OFF();
+    common.OFF();
 }
+
+void SevenSegment::DisplayNumber(int number)
+{
+    switch (number)
+    {
+    case 0:
+          DisplayNum0(); 
+        break;
+    case 1:
+          DisplayNum1(); 
+           break;
+    case 2:
+          DisplayNum2(); 
+           break;
+    case 3:
+          DisplayNum3(); 
+    
+        break;
+        // Todo: add case 4 to 9
+    }
+}
+
+void SevenSegment::DisplayNum0()
+{ 
+     common.OFF();
+     seg_a.ON();
+     seg_b.ON();
+     seg_c.ON();
+     seg_d.ON();
+     seg_e.ON();
+     seg_f.ON();
+     seg_g.OFF();
+     common.ON();
+    
+}
+
+void SevenSegment::DisplayNum1()
+{ 
+     common.OFF();
+     seg_a.OFF();
+     seg_b.ON();
+     seg_c.ON();
+     seg_d.OFF();
+     seg_e.OFF();
+     seg_f.OFF();
+     seg_g.OFF();
+     common.ON();
+    
+
+
+}
+
+void SevenSegment::DisplayNum9()
+{ 
+     common.OFF();
+     seg_a.ON();
+     seg_b.ON();
+     seg_c.ON();
+     seg_d.ON();
+     seg_e.OFF();
+     seg_f.ON();
+     seg_g.ON();
+     common.ON();
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main/main.cpp
+++ main/main.cpp
@@ -8,13 +8,48 @@ extern "C" void app_main(void)
 {
     while(1)
     {
-        s1.HardwareTest();
+        s1.DisplayNum0();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum1();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum2();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum3();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum4();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum5();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum6();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum7();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum8();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum9();
         vTaskDelay(500/portTICK_PERIOD_MS);
         s1.DisplayBlank();
+
+        s2.DisplayNum0();
         vTaskDelay(500/portTICK_PERIOD_MS);
-        s2.HardwareTest();
+        s2.DisplayNum1();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum2();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum3();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum4();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum5();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum6();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum7();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum8();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum9();
         vTaskDelay(500/portTICK_PERIOD_MS);
         s2.DisplayBlank();
-        vTaskDelay(500/portTICK_PERIOD_MS);
     } 
 }
